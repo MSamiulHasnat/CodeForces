e747d3b346b27709e2c1d8435d7207e3758d4f4b
samiulhsnt <samiulhsnt2016@gmail.com>
2022-06-13 20:42:22 +0600

    more problem

---
+++ ICPC Assiut Sheet2/F-MultiplicationTable.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+    
+    for(int i=1; i<=12; i++){
+        printf("%d * %d = %d\n", n, i, n*i);
+    }
+
+    return 0;
+}
