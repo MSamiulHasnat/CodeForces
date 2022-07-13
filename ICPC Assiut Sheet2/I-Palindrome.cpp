9a1a0f9808448823fe0909916630a9a067335b56
samiulhsnt <samiulhsnt2016@gmail.com>
2022-07-13 20:11:19 +0600

    Palindrome

---
+++ ICPC Assiut Sheet2/I-Palindrome.cpp
@@ -0,0 +1,26 @@
+// problem link - https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/I
+
+#include <iostream>
+
+#define nl '\n'
+
+
+using namespace std;
+
+int main(){
+
+    int num, mun = 0;
+    cin >> num;
+    int n = num;
+
+    while(num != 0){
+        int rem = num % 10;
+        mun = mun * 10 + rem;
+        num /= 10;
+    }
+    cout << mun << nl;
+    if(n == mun) cout <<"YES\n";
+    else cout << "NO\n";
+
+    return 0;
+}
